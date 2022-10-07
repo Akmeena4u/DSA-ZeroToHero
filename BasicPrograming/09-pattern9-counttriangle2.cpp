afe235fdfeb6bd62c2f3cbd8b3b561c6c41d04b3
Akmeena4u <93425334+Akmeena4u@users.noreply.github.com>
2022-10-07 14:53:40 +0530

    Update 09-pattern9-counttriangle2.cpp

--- BasicPrograming/09-pattern9-counttriangle2.cpp
+++ BasicPrograming/09-pattern9-counttriangle2.cpp
@@ -34,3 +34,35 @@ int main()
 
     return 0;
 }
+
+//approch 2
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    
+    int i=1;
+    
+    
+    while(i<=n){
+        int j=1;
+        int value=i;
+        while(j<=i){
+            cout<<value;
+            value++;
+            j++;
+        }
+        
+        cout<<endl;
+        i++;
+        
+    }
+    
+
+    return 0;
+}
