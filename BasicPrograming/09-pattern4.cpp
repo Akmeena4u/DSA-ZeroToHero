b5ef34deefa8251a55265c7d511307cc644a4d3a
Akmeena4u <93425334+Akmeena4u@users.noreply.github.com>
2022-10-07 14:11:25 +0530

    Update 09-pattern4.cpp

--- BasicPrograming/09-pattern4.cpp
+++ BasicPrograming/09-pattern4.cpp
@@ -36,3 +36,36 @@ main ()
 
   return 0;
 }
+
+
+//2nd approch
+
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+    
+    int n;
+    cin>>n;
+    int i=1;
+    
+    int count=0;
+    
+    while(i<=n){
+        int j=1;
+        
+        while(j<=n){
+            cout<<count+j;
+            
+            j++;
+        }
+        
+        cout<<endl;
+        i++;
+    }
+
+    return 0;
+}
