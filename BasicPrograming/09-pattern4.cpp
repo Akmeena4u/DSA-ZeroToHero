0fe1727e46058ca34b17fc9e820d70b17f30deb5
Akmeena4u <93425334+Akmeena4u@users.noreply.github.com>
2022-10-07 13:57:02 +0530

    Create 09-pattern4.cpp

---
+++ BasicPrograming/09-pattern4.cpp
@@ -0,0 +1,38 @@
+/*
+
+pattern 5
+
+12345
+12345
+12345
+12345
+12345
+  
+  */
+
+#include <iostream>
+using namespace std;
+
+int
+main ()
+{
+  int n;
+  cin >> n;
+
+  int i = 1;
+
+  while (i <= n)
+    {
+      int j = 1;
+      while (j <= n)
+	{
+	  cout << j;
+	  j++;
+	}
+
+      cout << endl;
+      i++;
+    }
+
+  return 0;
+}
