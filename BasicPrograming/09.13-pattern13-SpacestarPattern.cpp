8c78a2295da8a01b19a343d0d89c8191763e3693
Akmeena4u <93425334+Akmeena4u@users.noreply.github.com>
2022-10-07 16:23:12 +0530

    Create 09.13-pattern13-SpacestarPattern.cpp

---
+++ BasicPrograming/09.13-pattern13-SpacestarPattern.cpp
@@ -0,0 +1,52 @@
+/*
+
+   *
+  **
+ ***
+****
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int
+main ()
+{
+  int n;
+  cin >> n;
+
+  int i = 1;
+
+
+  while (i <= n)
+    {
+      int space = n-i;
+
+
+      while (space)
+	{
+	    
+	  cout << " ";
+	  space--;
+
+	  
+	}
+	
+	int j=1;
+	
+	while(j<=i){
+	    cout<<"*";
+	    
+	    j++;
+	}
+
+      cout << endl;
+      i++;
+
+    }
+
+
+  return 0;
+}
