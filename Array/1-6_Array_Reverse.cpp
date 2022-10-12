6d68767c1fb518be1ea77f3e7a8ee1fe488d3309
Akmeena4u <93425334+Akmeena4u@users.noreply.github.com>
2022-10-12 07:37:45 +0530

    Create 1-6_Array_Reverse.cpp

---
+++ Array/1-6_Array_Reverse.cpp
@@ -0,0 +1,44 @@
+/*
+
+To reverse an array Move element at first index to last, and element at last index to first Move element at second index to second last, and element at second last index to second
+By settimg start and end two variables , start++ end-- while (start>end)
+
+
+*/
+#include<iostream>
+using namespace std;
+
+void reverse(int arr[], int n) {
+
+    int start = 0;
+    int end = n-1;
+
+    while(start<=end) {
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+}
+
+void printArray(int arr[], int n) {
+    
+    for(int i=0; i<n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+
+    int arr[6] = {1,4,0,5,-2,15};
+    int brr[5] = {2,6,3,9,4};
+
+    reverse(arr, 6);
+    reverse(brr, 5);
+
+    printArray(arr, 6);
+    printArray(brr, 5);
+
+
+    return 0;
+}
