5316b0ab1c1c1a27219f62307d57c678610d5ec7
Akmeena4u <93425334+Akmeena4u@users.noreply.github.com>
2022-10-12 07:31:31 +0530

    Create 1-5_Array_LinearSearch.cpp

---
+++ Array/1-5_Array_LinearSearch.cpp
@@ -0,0 +1,45 @@
+/*
+
+A Linear Search also known as Sequential Search is a searching technique used in C++ to search an element from an array in a linear fashion. In this searching technique, an element is searched in sequential order one by one in an array from start to end.
+
+
+*/
+
+
+#include<iostream>
+using namespace std;
+
+bool search(int arr[], int size, int key) {
+
+    for( int i = 0; i<size; i++ ) {
+
+        if( arr[i] == key) {
+            return 1;
+        }
+
+    }
+    return 0;
+}
+
+
+int main() {
+
+    int arr[10] = { 5, 7, -2, 10, 22, -2, 0, 5, 22, 1};
+
+    cout <<" Enter the element to search for " << endl; 
+    int key;
+    cin >> key;
+
+    bool found = search(arr, 10, key);
+
+    if( found ) {
+        cout <<" Key is present "<< endl;
+    }
+    else{
+        cout <<" Key is absent " << endl;
+    }
+
+
+    return 0;
+}
+
