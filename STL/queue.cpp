29124f1416bc6d0d34d24591af530f4551b27858
Akmeena4u <93425334+Akmeena4u@users.noreply.github.com>
2022-10-28 16:15:43 +0530

    Create queue.cpp

---
+++ STL/queue.cpp
@@ -0,0 +1,33 @@
+/*
+
+A queue is a linear list of elements in which deletions can take place only at one end called the front, and insertions can take place only at the end called the rear. 
+The queue is a First In First Out type of data structure (FIFO),
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+void printqueue(queue<int> q1)
+{
+    queue<int> q2=q1;
+    while(!q2.empty())
+    {
+        cout<<q2.front()<<"\n";
+        q2.pop();
+    }
+}
+int main()
+{
+    queue<int> q;
+    for(int i=1;i<=5;i++)
+    q.push(i);
+    
+    cout<<"The elements of the queue are:"<<endl;
+    printqueue(q);
+    
+    cout<<"The size of the queue: "<<q.size()<<endl;
+    cout<<"The front element of the queue: "<<q.front()<<endl;
+    cout<<"The last element of the queue: "<<q.back()<<endl;
+    cout<<"Pop the front element: "<<endl;
+    q.pop();
+    printqueue(q);
+}
