bfbfe97624f8842f2db6137d3d55501e824e6c9b
Akmeena4u <93425334+Akmeena4u@users.noreply.github.com>
2022-10-28 16:10:44 +0530

    Create deque.cpp

---
+++ STL/deque.cpp
@@ -0,0 +1,41 @@
+/*
+
+Double Ended Queue which is also called Deque is a type of queue data structure in which insertion and deletion of elements can be either in front or rear. 
+
+it follow FILO RULE
+
+*/
+#include<bits/stdc++.h>
+using namespace std;
+void printdeque(deque<int> dq)
+{
+    deque<int>::iterator it;
+    for(it=dq.begin();it!=dq.end();it++)
+    {
+        cout<<*it<<" ";
+    }
+    cout<<endl;
+}
+int main()
+{
+    deque<int> dq;
+    dq.push_back(10);
+    dq.push_back(20);
+    dq.push_front(30);
+    dq.push_front(40);
+    dq.push_front(50);
+    
+    cout<<"The elements in the deque are: ";
+    printdeque(dq);
+    
+    cout<<"The size of the deque is: "<<dq.size()<<endl;
+    cout<<"The first element in the deque: "<<dq.front()<<endl;
+    cout<<"Deleting the first element"<<endl;
+    dq.pop_front();
+    printdeque(dq);
+    cout<<"The last element of the deque: "<<dq.back()<<endl;
+    cout<<"Deleting the last element"<<endl;
+    dq.pop_back();
+    printdeque(dq);
+    
+}
