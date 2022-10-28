30573f2c4cc7a5652a77000bb2319ffa22820464
Akmeena4u <93425334+Akmeena4u@users.noreply.github.com>
2022-10-28 17:19:42 +0530

    Create map.cpp

---
+++ STL/map.cpp
@@ -0,0 +1,43 @@
+/*
+
+map in STL are associative containers where each element consists of a key value and a mapped value. Two mapped values cannot have the same key value.
+
+all key will be in sorted & unique
+
+*/
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  map < int, int > mp;
+  for (int i = 1; i <= 5; i++) {
+    mp.insert({i , i * 10});
+  }
+
+  cout << "Elements present in the map: " << endl;
+  cout << "Key\tElement" << endl;
+  for (auto it = mp.begin(); it != mp.end(); it++) {
+    cout << it -> first << "\t" << it -> second << endl;
+  }
+
+  int n = 2;
+  if (mp.find(2) != mp.end())
+    cout << n << " is present in map" << endl;
+
+  mp.erase(mp.begin());
+  cout << "Elements after deleting the first element: " << endl;
+  cout << "Key\tElement" << endl;
+  for (auto it = mp.begin(); it != mp.end(); it++) {
+    cout << it -> first << "\t" << it -> second << endl;
+  }
+
+  cout << "The size of the map is: " << mp.size() << endl;
+
+  if (mp.empty() == false)
+    cout << "The map is not empty " << endl;
+  else
+    cout << "The map is empty" << endl;
+  mp.clear();
+  cout << "Size of the map after clearing all the elements: " << mp.size();
+}
