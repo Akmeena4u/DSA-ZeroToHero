9dc943f1d72a2c5134f6000a560bcf9d83b9d662
Amit kumar meena <93425334+Akmeena4u@users.noreply.github.com>
2023-07-17 23:22:30 +0530

    Update MajorityElement_AppearMoreThan_N_By_2.cpp

--- Array/MajorityElement_AppearMoreThan_N_By_2.cpp
+++ Array/MajorityElement_AppearMoreThan_N_By_2.cpp
@@ -23,3 +23,32 @@ int majorityElement(int a[], int size)
     return -1;
         
     }
+
+//optimal app === Moore’s Voting Algorithm:
+int majorityElement(vector<int> v) {
+
+    //size of the given array:
+    int n = v.size();
+    int cnt = 0; // count
+    int el; // Element
+
+    //applying the algorithm:
+    for (int i = 0; i < n; i++) {
+        if (cnt == 0) {
+            cnt = 1;
+            el = v[i];
+        }
+        else if (el == v[i]) cnt++;
+        else cnt--;
+    }
+
+    //checking if the stored element
+    // is the majority element:
+    int cnt1 = 0;
+    for (int i = 0; i < n; i++) {
+        if (v[i] == el) cnt1++;
+    }
+
+    if (cnt1 > (n / 2)) return el;
+    return -1;
+}
