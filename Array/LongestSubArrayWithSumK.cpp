b4ac24de01c220198b44f9a8e6128786c15a5a44
Amit kumar meena <93425334+Akmeena4u@users.noreply.github.com>
2023-07-17 23:12:44 +0530

    Create LongestSubArrayWithSumK.cpp

---
+++ Array/LongestSubArrayWithSumK.cpp
@@ -0,0 +1,102 @@
+//app -1 ---Brute force
+int getLongestSubarray(vector<int>& a, long long k) {
+    int n = a.size(); // size of the array.
+
+    int len = 0;
+    for (int i = 0; i < n; i++) { // starting index
+        for (int j = i; j < n; j++) { // ending index
+            // add all the elements of
+            // subarray = a[i...j]:
+            long long s = 0;
+            for (int K = i; K <= j; K++) {
+                s += a[K];
+            }
+
+            if (s == k)
+                len = max(len, j - i + 1);
+        }
+    }
+    return len;
+}
+
+// app 2 -- Bruteforce2
+int getLongestSubarray(vector<int>& a, long long k) {
+    int n = a.size(); // size of the array.
+
+    int len = 0;
+    for (int i = 0; i < n; i++) { // starting index
+        long long s = 0; // Sum variable
+        for (int j = i; j < n; j++) { // ending index
+            // add the current element to
+            // the subarray a[i...j-1]:
+            s += a[j];
+
+            if (s == k)
+                len = max(len, j - i + 1);
+        }
+    }
+    return len;
+}
+
+// app 3----TWO POINTER app (+ve num only)
+int getLongestSubarray(vector<int>& a, long long k) {
+    int n = a.size(); // size of the array.
+
+    int left = 0, right = 0; // 2 pointers
+    long long sum = a[0];
+    int maxLen = 0;
+    while (right < n) {
+        // if sum > k, reduce the subarray from left
+        // until sum becomes less or equal to k:
+        while (left <= right && sum > k) {
+            sum -= a[left];
+            left++;
+        }
+
+        // if sum = k, update the maxLen i.e. answer:
+        if (sum == k) {
+            maxLen = max(maxLen, right - left + 1);
+        }
+
+        // Move forward thw right pointer:
+        right++;
+        if (right < n) sum += a[right];
+    }
+
+    return maxLen;
+}
+
+//app 4 Hashing ( for all integer)
+
+int getLongestSubarray(vector<int>& a, long long k) {
+    int n = a.size(); // size of the array.
+
+    map<long long, int> preSumMap;
+    long long sum = 0;
+    int maxLen = 0;
+    for (int i = 0; i < n; i++) {
+        //calculate the prefix sum till index i:
+        sum += a[i];
+
+        // if the sum = k, update the maxLen:
+        if (sum == k) {
+            maxLen = max(maxLen, i + 1);
+        }
+
+        // calculate the sum of remaining part i.e. x-k:
+        long long rem = sum - k;
+
+        //Calculate the length and update maxLen:
+        if (preSumMap.find(rem) != preSumMap.end()) {
+            int len = i - preSumMap[rem];
+            maxLen = max(maxLen, len);
+        }
+
+        //Finally, update the map checking the conditions:
+        if (preSumMap.find(sum) == preSumMap.end()) {
+            preSumMap[sum] = i;
+        }
+    }
+
+    return maxLen;
+}
