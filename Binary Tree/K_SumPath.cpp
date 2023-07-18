4c6b01b0fdbe80cba86cd221f842bc16a3204de9
Amit kumar meena <93425334+Akmeena4u@users.noreply.github.com>
2023-07-18 18:52:52 +0530

    Create K_SumPath.cpp

---
+++ Binary Tree/K_SumPath.cpp
@@ -0,0 +1,34 @@
+//Given a binary tree and an integer K. Find the number of paths in the tree which have their sum equal to K.
+//A path may start from any node and end at any node in the downward direction.
+
+void solve(Node* root, int k, int &count, vector<int> &path) {
+        //base case
+        if(root == NULL)
+            return ;
+        
+        path.push_back(root->data);
+        
+        //left
+        solve(root->left, k, count, path);
+        //right
+        solve(root->right, k, count, path);
+        
+        //check for K Sum
+        
+        int size = path.size();
+        int sum = 0;
+        for(int i = size-1; i>=0; i--)  {
+            sum += path[i];
+            if(sum == k)
+                count++;
+        }
+        
+        path.pop_back();
+    }
+    int sumK(Node *root,int k)
+    {
+        vector<int> path;
+        int count = 0;
+        solve(root, k, count, path);
+        return count;
+    }
