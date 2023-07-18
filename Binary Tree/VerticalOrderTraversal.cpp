11ad08ceab3370c7cece4c24632b9091149d8493
Amit kumar meena <93425334+Akmeena4u@users.noreply.github.com>
2023-07-18 22:10:57 +0530

    Create VerticalOrderTraversal.cpp

---
+++ Binary Tree/VerticalOrderTraversal.cpp
@@ -0,0 +1,41 @@
+//Function to find the vertical order traversal of Binary Tree.
+    vector<int> verticalOrder(Node *root)
+    {
+        //Your code here
+          map<int, map<int,vector<int> > > nodes;
+        queue< pair<Node*, pair<int,int> > > q;
+        vector<int> ans;
+        
+        if(root == NULL)
+            return ans;
+            
+        q.push(make_pair(root, make_pair(0,0)));
+        
+        while(!q.empty()) {
+            pair<Node*, pair<int,int> > temp = q.front();
+            q.pop();
+            Node* frontNode = temp.first;
+            int hd = temp.second.first;
+            int lvl = temp.second.second;
+            
+            nodes[hd][lvl].push_back(frontNode->data);
+            
+            if(frontNode->left)
+                q.push(make_pair(frontNode->left, make_pair(hd-1, lvl+1) ));
+                
+            if(frontNode->right)
+                q.push(make_pair(frontNode->right, make_pair(hd+1, lvl+1)));
+        }
+        
+        for(auto i: nodes) {
+            
+            for(auto j:i.second) {
+                
+                for(auto k:j.second)
+                {
+                    ans.push_back(k);
+                }
+            }
+        }
+        return ans;
+    }
