a5c52d3591c7dbbb6f24dbf8911472c37ac1b2dc
Amit kumar meena <93425334+Akmeena4u@users.noreply.github.com>
2023-07-19 11:30:56 +0530

    Create LCA_BST.cpp

---
+++ BinarySearchTree/LCA_BST.cpp
@@ -0,0 +1,16 @@
+ TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        // Base case
+        if(root == NULL)
+            return NULL;
+        // Case 1 : Answer is in left
+        if(p->val < root->val && q->val < root->val){
+            return lowestCommonAncestor(root->left,p,q);
+        }
+        // Case 2 : Answer is in right
+        else if(p->val > root->val && q->val > root->val){
+            return lowestCommonAncestor(root->right,p,q);
+        }
+        // Case 3 : Answer is root
+        else
+            return root;
+    }
