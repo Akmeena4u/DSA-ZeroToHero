219e761582d6818dddb6c1bbb71cb28ea63db063
Amit kumar meena <93425334+Akmeena4u@users.noreply.github.com>
2023-07-19 09:54:10 +0530

    Create FlattenBTToLL.cpp

---
+++ Binary Tree/FlattenBTToLL.cpp
@@ -0,0 +1,20 @@
+ void flatten(Node *root)
+    {
+        //code here
+        Node* curr=root;
+        
+        while(curr){
+            if(curr->left){
+                Node* pred=curr->left;
+                while(pred->right){
+                    pred=pred->right;
+                }
+                
+                pred->right=curr->right;
+                curr->right=curr->left;
+                curr->left=NULL;
+            }
+            
+            curr=curr->right;
+        }
+    }
