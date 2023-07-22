2229e5a8a8a492c8cf33249bada7e004e5e7432c
Amit kumar meena <93425334+Akmeena4u@users.noreply.github.com>
2023-07-22 15:45:32 +0530

    Create RemoveDuplicates_From_SortedllLL.cpp

---
+++ Linked List/RemoveDuplicates_From_SortedllLL.cpp
@@ -0,0 +1,26 @@
+Node *removeDuplicates(Node *head)
+{
+ // your code goes here
+ //empty List
+    if(head == NULL)
+        return NULL;
+    
+    //non empty list
+    Node* curr = head;
+    
+    while(curr != NULL) {
+        
+        if( (curr -> next != NULL) && curr -> data == curr -> next -> data) {
+            Node* next_next = curr ->next -> next;
+            Node* nodeToDelete = curr -> next;
+            delete(nodeToDelete);
+            curr -> next = next_next;
+        }
+        else //not equal
+        {
+            curr = curr -> next;
+        }   
+    }
+    
+    return head; 
+}
