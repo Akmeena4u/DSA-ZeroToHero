d1e48e15da3310516d1bba033fe07cb71ba94a47
Amit kumar meena <93425334+Akmeena4u@users.noreply.github.com>
2023-07-26 12:47:45 +0530

    Update KthAncestorBT.cpp

--- Binary Tree/KthAncestorBT.cpp
+++ Binary Tree/KthAncestorBT.cpp
@@ -26,3 +26,43 @@ int kthAncestor(Node *root, int k, int node)
     // If the key node is not found in the subtrees, return -1
     return -1;
 }
+
+----------------------------------------------------------------------------------
+
+    // your task is to complete this function
+vector<int>temp;
+
+bool solve(Node* root, int val){
+    
+    if(root==NULL)   return false;
+    
+    if(root->data == val)   return true;
+    
+    bool left = solve(root->left, val);
+    
+    bool right = solve(root->right, val);
+    
+    if(left || right)   {
+        
+        temp.push_back(root->data); //node mil chuka h or waps jate time all ansestor ko temp me dalte jayenge
+        
+        return true;
+    }
+    
+    return false;
+    
+}
+
+int kthAncestor(Node *root, int k, int node)
+{
+    // Code here
+    
+    temp.clear();
+    
+    bool call = solve(root, node);
+    
+    if(temp.size() < k) return -1;  //means there are not enough ancestors to find the k-th ancestor, 
+    
+    return temp[k - 1];
+}
+
