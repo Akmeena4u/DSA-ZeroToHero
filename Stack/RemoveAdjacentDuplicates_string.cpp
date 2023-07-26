804db939660eb02c61141c1ca8bf587475550c6e
Amit kumar meena <93425334+Akmeena4u@users.noreply.github.com>
2023-07-26 11:56:46 +0530

    Create RemoveAdjacentDuplicates_string.cpp

---
+++ Stack/RemoveAdjacentDuplicates_string.cpp
@@ -0,0 +1,16 @@
+ string removeDuplicates(string s) {
+        stack<char> st;
+        for(auto ch:s){
+               
+               if(st.empty())  st.push(ch);
+               else if(!st.empty() && st.top()==ch)  st.pop();
+               else st.push(ch);
+        }
+        string ans;
+        while(!st.empty()){
+             ans+=st.top();
+             st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
