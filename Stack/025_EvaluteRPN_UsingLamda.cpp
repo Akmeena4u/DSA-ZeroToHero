60a31526b5512bda8b0ad4f706661f84579be58d
Amit kumar meena <93425334+Akmeena4u@users.noreply.github.com>
2023-07-28 17:14:13 +0530

    Create 025_EvaluteRPN_UsingLamda.cpp

---
+++ Stack/025_EvaluteRPN_UsingLamda.cpp
@@ -0,0 +1,33 @@
+/*
+    Using fancy Lambda on unordered_map
+    
+*/
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> st;
+        int result = 0;
+        
+        unordered_map<string, function<int (int, int)> > mp = {
+            {"+", [](int a, int b) {return a + b; } },
+            {"-", [](int a, int b) {return a - b; } },
+            {"*", [](int a, int b) {return (long)a * (long)b; } },
+            {"/", [](int a, int b) {return a / b; } },
+        };
+        
+        for(const string& s:tokens) {
+            if(s == "+" || s == "-" || s == "*" || s == "/") {
+                int b = st.top();
+                st.pop();
+                int a = st.top();
+                st.pop();
+                
+                result = mp[s](a, b);
+                st.push(result);
+            } else {
+                st.push(stoi(s));
+            }
+        }
+        return st.top();
+    }
+};
