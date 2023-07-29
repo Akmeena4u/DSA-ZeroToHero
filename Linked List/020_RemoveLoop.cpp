6e65f0a1bd7715cdd59d455a65f732b26424d3b3
Amit kumar meena <93425334+Akmeena4u@users.noreply.github.com>
2023-07-29 17:38:06 +0530

    Create 020_RemoveLoop.cpp

---
+++ Linked List/020_RemoveLoop.cpp
@@ -0,0 +1,30 @@
+
+void removeLoop(Node* &head){
+    if(head == NULL){
+        return;
+    }
+    Node* slow = head;
+    Node* fast = head;
+    while(fast != NULL){
+        fast = fast -> next;
+        if(fast != NULL){
+            fast = fast->next;
+            slow = slow->next;
+        }
+        // loop is present
+        if(slow == fast){
+            // Move slow to head
+            slow = head;
+            break;
+        }
+    }
+    Node *prev = fast;
+    while(slow!=fast){
+        // Move both by 1 step
+        prev = fast;
+        slow = slow->next;
+        fast = fast->next;
+    }
+    // Removal step
+    prev->next = NULL;
+}
