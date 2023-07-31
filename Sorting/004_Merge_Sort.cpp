69bf59f748479c377b4536f1a23f883bab7f90e1
Amit kumar meena <93425334+Akmeena4u@users.noreply.github.com>
2023-07-31 23:44:32 +0530

    Create 004_Merge_Sort.cpp

---
+++ Sorting/004_Merge_Sort.cpp
@@ -0,0 +1,98 @@
+/*
+
+Merge sort is a popular divide-and-conquer sorting algorithm that divides the input array into smaller halves, 
+sorts them separately, and then merges the sorted halves to produce a fully sorted array.
+
+The basic idea of the merge sort algorithm is as follows:
+
+Divide the unsorted array into two halves.
+Recursively sort each half separately by applying the merge sort algorithm.
+Merge the two sorted halves into a single sorted array. 
+
+  */
+
+
+#include<iostream>
+using namespace std;
+
+void merge(int *arr, int s, int e) {
+
+    int mid = (s+e)/2;
+
+    int len1 = mid - s + 1;
+    int len2 = e - mid;
+
+    int *first = new int[len1];
+    int *second = new int[len2];
+
+    //copy values
+    int mainArrayIndex = s;
+    for(int i=0; i<len1; i++) {
+        first[i] = arr[mainArrayIndex++];
+    }
+
+    mainArrayIndex = mid+1;
+    for(int i=0; i<len2; i++) {
+        second[i] = arr[mainArrayIndex++];
+    }
+
+    //merge 2 sorted arrays     
+    int index1 = 0;
+    int index2 = 0;
+    mainArrayIndex = s;
+
+    while(index1 < len1 && index2 < len2) {
+        if(first[index1] < second[index2]) {
+            arr[mainArrayIndex++] = first[index1++];
+        }
+        else{
+            arr[mainArrayIndex++] = second[index2++];
+        }
+    }   
+
+    while(index1 < len1) {
+        arr[mainArrayIndex++] = first[index1++];
+    }
+
+    while(index2 < len2 ) {
+        arr[mainArrayIndex++] = second[index2++];
+    }
+
+    delete []first;
+    delete []second;
+
+}
+
+void mergeSort(int *arr, int s, int e) {
+
+    //base case
+    if(s >= e) {
+        return;
+    }
+    
+    int mid = (s+e)/2;
+
+    //left part sort karna h 
+    mergeSort(arr, s, mid);
+    
+    //right part sort karna h 
+    mergeSort(arr, mid+1, e);
+
+    //merge
+    merge(arr, s, e);
+
+}
+
+int main() {
+
+    int arr[15] = {3,7,0,1,5,8,3,2,34,66,87,23,12,12,12};
+    int n = 15;
+
+    mergeSort(arr, 0, n-1);
+
+    for(int i=0;i<n;i++){
+        cout << arr[i] << " ";
+    } cout << endl;
+
+    return 0;
+}
