2f811d5fce7ec30b607301db2fc78ffbf6609031
Amit kumar meena <93425334+Akmeena4u@users.noreply.github.com>
2023-08-04 18:06:33 +0530

    Create 004_ClimbingStairs_1D_DP.cpp

---
+++ Dynamic Programing/004_ClimbingStairs_1D_DP.cpp
@@ -0,0 +1,42 @@
+//Bottom up
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+
+  int n=3;
+  vector<int> dp(n+1,-1);
+  
+  dp[0]= 1;
+  dp[1]= 1;
+  
+  for(int i=2; i<=n; i++){
+      dp[i] = dp[i-1]+ dp[i-2];
+  }
+  cout<<dp[n];
+  return 0;
+}
+
+//Space optimization
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+
+  int n=3;
+  
+  int prev2 = 1;
+  int prev = 1;
+  
+  for(int i=2; i<=n; i++){
+      int cur_i = prev2+ prev;
+      prev2 = prev;
+      prev= cur_i;
+  }
+  cout<<prev;
+  return 0;
+}
