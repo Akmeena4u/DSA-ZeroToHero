624a6877d737066091ee000a1a75b3abc9c19107
Amit kumar meena <93425334+Akmeena4u@users.noreply.github.com>
2023-08-08 09:27:42 +0530

    Create 004_RemoveDuplicatesFromSortedArray_Inplace.cpp

---
+++ TwoPointers/004_RemoveDuplicatesFromSortedArray_Inplace.cpp
@@ -0,0 +1,21 @@
+// Input: nums = [0,0,1,1,1,2,2,3,3,4]
+// Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+// Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+// It does not matter what you leave beyond the returned k (hence they are underscores).
+
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if(nums.size()==0)  return 0;
+        int i=0;
+        for(int j=1;j<nums.size();j++){
+            if(nums[j]!=nums[i]){
+                i++;
+                nums[i]=nums[j];
+            }
+        }
+            return i+1;
+
+    }
+};
