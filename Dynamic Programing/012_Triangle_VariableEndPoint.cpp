3a2119a1f6de55ee5627b0f7ad2638694b359e8a
Amit kumar meena <93425334+Akmeena4u@users.noreply.github.com>
2023-08-12 16:07:15 +0530

    Create 012_Triangle_VariableEndPoint.cpp

---
+++ Dynamic Programing/012_Triangle_VariableEndPoint.cpp
@@ -0,0 +1,84 @@
+//Top down
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int minimumPathSumUtil(int i, int j, vector<vector<int> > &triangle,int n, 
+vector<vector<int> > &dp) {
+  
+  if(dp[i][j]!=-1)
+  return dp[i][j];
+  
+  if(i==n-1) return triangle[i][j];
+    
+  int down = triangle[i][j]+minimumPathSumUtil(i+1,j,triangle,n,dp);
+  int diagonal = triangle[i][j]+minimumPathSumUtil(i+1,j+1,triangle,n,dp);
+  
+  return dp[i][j] = min(down, diagonal);
+  
+}
+
+int minimumPathSum(vector<vector<int> > &triangle, int n){
+    vector<vector<int> > dp(n,vector<int>(n,-1));
+    return minimumPathSumUtil(0,0,triangle,n,dp);
+    
+}
+
+int main() {
+
+  vector<vector<int> > triangle{{1},
+                                {2,3},
+                                {3,6,7},
+                                {8,9,6,10}};
+                            
+  int n = triangle.size();
+  
+  cout<<minimumPathSum(triangle,n);
+}
+
+// tabulation------------------------------------------
+
+int minimumPathSum(vector<vector<int> > &triangle, int n){
+    vector<vector<int> > dp(n,vector<int>(n,0));
+    
+    for(int j=0;j<n;j++){
+        dp[n-1][j] = triangle[n-1][j];
+    }
+    
+    for(int i=n-2; i>=0; i--){
+        for(int j=i; j>=0; j--){
+            
+            int down = triangle[i][j]+dp[i+1][j];
+            int diagonal = triangle[i][j]+dp[i+1][j+1];
+            
+            dp[i][j] = min(down, diagonal);
+        }
+    }
+    
+    return dp[0][0];
+    
+}
+
+//space optimizaed soln--------------------
+
+int minimumPathSum(vector<vector<int> > &triangle, int n){
+    vector<int> front(n,0), cur(n,0);
+    
+    for(int j=0;j<n;j++){
+        front[j] = triangle[n-1][j];
+    }
+    
+    for(int i=n-2; i>=0; i--){
+        for(int j=i; j>=0; j--){
+            
+            int down = triangle[i][j]+front[j];
+            int diagonal = triangle[i][j]+front[j+1];
+            
+            cur[j] = min(down, diagonal);
+        }
+        front=cur;
+    }
+    
+    return front[0];
+    
+}
