65a552c126ae814e37e3f7eefbb00d94300ee3c7
Amit kumar meena <93425334+Akmeena4u@users.noreply.github.com>
2023-08-22 17:48:46 +0530

    Create 018_CountSubsetWithGivenSumK_DpOnSubsequence.cpp

---
+++ Dynamic Programing/018_CountSubsetWithGivenSumK_DpOnSubsequence.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int findWaysUtil(int ind, int target, vector<int>& arr, vector<vector<int>> &dp){
+    if(target==0)
+        return 1;
+    
+    if(ind == 0)
+        return arr[0] == target;
+    
+    if(dp[ind][target]!=-1)
+        return dp[ind][target];
+        
+    int notTaken = findWaysUtil(ind-1,target,arr,dp);
+    
+    int taken = 0;
+    if(arr[ind]<=target)
+        taken = findWaysUtil(ind-1,target-arr[ind],arr,dp);
+        
+    return dp[ind][target]= notTaken + taken;
+}
+
+int findWays(vector<int> &num, int k){
+    int n = num.size();
+    vector<vector<int>> dp(n,vector<int>(k+1,-1));
+    return findWaysUtil(n-1,k,num,dp);
+}
+
+int main() {
+
+  vector<int> arr = {1,2,2,3};
+  int k=3;
+                                 
+  cout<<"The number of subsets found are " <<findWays(arr,k);
+}
+
+
+// Time Complexity: O(N*K)
+
+// Reason: There are N*K states therefore at max ‘N*K’ new problems will be solved.
+
+// Space Complexity: O(N*K) + O(N)
+
+// Reason: We are using a recursion stack space(O(N)) and a 2D array ( O(N*K)).
+
+//--------------------------------------------------------Tabulation----------------------
+
+
+int findWays(vector<int> &num, int k){
+     int n = num.size();
+
+    vector<vector<int>> dp(n,vector<int>(k+1,0));
+    
+    for(int i=0; i<n; i++){
+        dp[i][0] = 1;
+    }
+    
+    if(num[0]<=k)
+        dp[0][num[0]] = 1;
+    
+    for(int ind = 1; ind<n; ind++){
+        for(int target= 1; target<=k; target++){
+            
+            int notTaken = dp[ind-1][target];
+    
+            int taken = 0;
+                if(num[ind]<=target)
+                    taken = dp[ind-1][target-num[ind]];
+        
+            dp[ind][target]= notTaken + taken;
+        }
+    }
+    
+    return dp[n-1][k];
+    
+}
