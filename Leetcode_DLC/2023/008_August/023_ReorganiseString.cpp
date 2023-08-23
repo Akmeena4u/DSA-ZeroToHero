463cb9ba6e8cd32b9efdd22e0406b4042b758d07
Amit kumar meena <93425334+Akmeena4u@users.noreply.github.com>
2023-08-23 21:08:53 +0530

    Create 023_ReorganiseString.cpp

---
+++ Leetcode_DLC/2023/008_August/023_ReorganiseString.cpp
@@ -0,0 +1,56 @@
+// Given a string s, rearrange the characters of s so that any two adjacent characters are not the same.
+
+string reorganizeString(string s) {
+
+        //create mapping
+        int freq[26] = {0};
+
+        for(int i=0; i<s.length(); i++) {
+            char ch = s[i];
+            freq[ch-'a']++;
+        }
+
+        priority_queue<node, vector<node>, compare> maxHeap;
+
+        for(int i=0; i<26; i++) {
+            if(freq[i] != 0) {
+                node temp(i+'a', freq[i]);
+                maxHeap.push(temp);
+            }
+        }
+
+        string ans = "";
+
+        while(maxHeap.size() > 1) {
+            node first = maxHeap.top();
+            maxHeap.pop();
+            node second = maxHeap.top();
+            maxHeap.pop();
+
+            ans += first.data;
+            ans += second.data;
+
+            first.count--;
+            second.count--;
+
+            if(first.count != 0) {
+                maxHeap.push(first);
+            } 
+
+            if(second.count != 0) {
+                maxHeap.push(second);
+            } 
+        }
+
+        if(maxHeap.size() == 1) {
+            node temp = maxHeap.top();
+            maxHeap.pop();
+            if(temp.count == 1) 
+                ans += temp.data;
+            else
+                return "";
+        }
+
+        return ans;
+        
+    }
