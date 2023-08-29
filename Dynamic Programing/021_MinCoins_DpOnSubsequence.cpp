a4afed83a8b634f487f68ff106a58818054bd7e2
Amit kumar meena <93425334+Akmeena4u@users.noreply.github.com>
2023-08-29 09:38:34 +0530

    Create 021_MinCoins_DpOnSubsequence.cpp

---
+++ Dynamic Programing/021_MinCoins_DpOnSubsequence.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int minimumElementsUtil(vector<int>& arr, int ind, int T, vector<vector<int>>& dp){
+
+    if(ind == 0){
+        if(T%arr[0] == 0) return T/arr[0];
+        else return 1e9;
+    }
+    
+    if(dp[ind][T]!=-1)
+        return dp[ind][T];
+        
+    int notTaken = 0 + minimumElementsUtil(arr,ind-1,T,dp);
+    
+    int taken = 1e9;
+    if(arr[ind] <= T)
+        taken = 1 + minimumElementsUtil(arr,ind,T-arr[ind],dp);    //importanat----infinite supply pattern ---yaha index kam nahi hogaaa
+        
+    return dp[ind][T] = min(notTaken,taken);
+}
+
+
+int minimumElements(vector<int>& arr, int T){
+    
+    int n= arr.size();
+    
+    vector<vector<int>> dp(n,vector<int>(T+1,-1));
+    int ans =  minimumElementsUtil(arr, n-1, T, dp);
+    if(ans >= 1e9) return -1;
+    return ans;
+}
+
+int main() {
+
+  vector<int> arr ={1,2,3};
+  int T=7;
+                                 
+  cout<<"The minimum number of coins required to form the target sum is " 
+  <<minimumElements(arr,T);
+}
+
+
+// Time Complexity: O(N*T)
+
+// Reason: There are N*T states therefore at max ‘N*T’ new problems will be solved.
+
+// Space Complexity: O(N*T) + O(N)
+
+// Reason: We are using a recursion stack space(O(N)) and a 2D array ( O(N*T)).
