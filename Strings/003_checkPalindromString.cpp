6bcbc422fc1ccede764c909755e9285d7c1159cc
Amit kumar meena <93425334+Akmeena4u@users.noreply.github.com>
2023-11-14 10:27:01 +0530

    Create 003_checkPalindromString.cpp

---
+++ Strings/003_checkPalindromString.cpp
@@ -0,0 +1,15 @@
+//User function template for C++
+class Solution{
+public:	
+	
+	
+	int isPalindrome(string S)
+	{
+	    // Your code goes here
+	     int n = S.size();
+     for(int i=0;i<n/2;i++)
+	    if(S[i]!=S[n-1-i])return 0;
+     return 1;
+	}
+
+};
