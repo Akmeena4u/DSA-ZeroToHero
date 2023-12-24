6ccdae7ae3478efe35dda0de0164a8b0a836ac99
Amit kumar meena <93425334+Akmeena4u@users.noreply.github.com>
2023-12-24 16:04:26 +0530

    Create 020_SubsetSum_i.cpp

---
+++ Recursion/020_SubsetSum_i.cpp
@@ -0,0 +1,42 @@
+/*
+Given a list arr of N integers, return sums of all subsets in it.
+
+ 
+
+Example 1:
+
+Input:
+N = 2
+arr[] = {2, 3}
+Output:
+0 2 3 5
+Explanation:
+When no elements is taken then Sum = 0.
+When only 2 is taken then Sum = 2.
+When only 3 is taken then Sum = 3.
+When element 2 and 3 are taken then 
+Sum = 2+3 = 5.
+
+  */
+
+
+
+
+
+
+void subsetSumsHelper(int ind, vector < int > & arr, int n, vector < int > & ans, int sum) {
+      if (ind == n) {
+        ans.push_back(sum);
+        return;
+      }
+      //element is picked
+      subsetSumsHelper(ind + 1, arr, n, ans, sum + arr[ind]);
+      //element is not picked
+      subsetSumsHelper(ind + 1, arr, n, ans, sum);
+    }
+  vector < int > subsetSums(vector < int > arr, int n) {
+    vector < int > ans;
+    subsetSumsHelper(0, arr, n, ans, 0);
+    sort(ans.begin(), ans.end());
+    return ans;
+  }
