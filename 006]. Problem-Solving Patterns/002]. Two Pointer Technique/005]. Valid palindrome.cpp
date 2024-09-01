67472e9178825d11c836a1b03d2715b9885499f7
Amit kumar meena <93425334+Akmeena4u@users.noreply.github.com>
2024-09-01 11:31:12 +0530

    Create 005]. Valid palindrome.cpp

---
+++ 006]. Problem-Solving Patterns/002]. Two Pointer Technique/005]. Valid palindrome.cpp
@@ -0,0 +1,37 @@
+/*
+
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters,
+it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+
+  */
+
+
+class Solution{
+public:	
+	bool sentencePalindrome(string s) 
+	{
+	    // code here 
+	     int start=0;
+       int end=s.size()-1;
+       while(start<=end){
+           if(!isalnum(s[start])){start++; continue;}
+           if(!isalnum(s[end])){end--;continue;}
+           if(tolower(s[start])!=tolower(s[end]))return false;
+           else{
+               start++;
+               end--;
+           }
+       }
+       return true;
+	}
+};
