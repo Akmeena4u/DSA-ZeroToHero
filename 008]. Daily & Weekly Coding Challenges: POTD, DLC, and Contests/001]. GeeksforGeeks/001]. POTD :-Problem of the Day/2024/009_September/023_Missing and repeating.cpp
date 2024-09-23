9877a4b0b9faee3e80a490987a1efd685723496f
Amit kumar meena <93425334+Akmeena4u@users.noreply.github.com>
2024-09-23 13:25:39 +0530

    Create 023_Missing and repeating.cpp

---
+++ 008]. Daily & Weekly Coding Challenges: POTD, DLC, and Contests/001]. GeeksforGeeks/001]. POTD :-Problem of the Day/2024/009_September/023_Missing and repeating.cpp
@@ -0,0 +1,21 @@
+ vector<int> findTwoElement(vector<int>& a) {
+          // code here
+          int n=a.size();
+        int hash[n + 1] = {0}; // hash array
+
+    //update the hash array:
+    for (int i = 0; i < n; i++) {
+        hash[a[i]]++;
+    }
+
+    //Find the repeating and missing number:
+    int repeating = -1, missing = -1;
+    for (int i = 1; i <= n; i++) {
+        if (hash[i] == 2) repeating = i;
+        else if (hash[i] == 0) missing = i;
+
+        if (repeating != -1 && missing != -1)
+            break;
+    }
+    return {repeating, missing};
+    }
